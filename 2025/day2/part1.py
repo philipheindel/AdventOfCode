1d83f116ff174b8111d9ebb35ebfd012a4c1d245
Philip Heindel <p.e.heindel@gmail.com>
2025-12-02 15:56:05 +0000

    Completed day2 parts 1 and 2

--- 2025/day2/part1.py
+++ 2025/day2/part1.py
@@ -1,32 +1,29 @@
-
 total = 0
 
 
-def contains_repeat(number: str, parts: int) -> bool:
-    half = len(number) / 2
-    if parts > half:
-        return False
-    if len(number) % parts != 0:
-        return contains_repeat(number, parts + 1)
-    parts_divided = [number[i:i+parts] for i in range(0, len(number), parts)]
-    if len(set(parts_divided)) == 1:
-        print(parts_divided)
-        return True
-    return contains_repeat(number, parts + 1)
+def contains_repeat(number: str) -> bool:
+    half = int(len(number) / 2)
+    half_1 = number[half:]
+    half_2 = number[:half]
+    return half_1 == half_2
         
 
 with open("input.txt") as file:
     for id_range in file.readline().split(','):
         range_start = int(id_range.split('-')[0])
-        range_end = int(id_range.split('-')[1])
+        range_end = int(id_range.split('-')[1]) + 1
         print(f"start:{range_start}\tend:{range_end}")
         for id in range(range_start, range_end):
             id_str = str(id)
-            if all(char == id_str[0] for char in id_str):
+            if all(char == id_str[0] for char in id_str) and (len(id_str) not in [1,3,5,7]):
+            #if len(id_str) % 2 != 0:
+                #print(f"{id} -> Invalid")
                 total += id
                 continue
-            if contains_repeat(id_str, 2):
-                #print(f"Adding {id} to total")
+            if contains_repeat(id_str):
+                #print(f"{id} -> Invalid")
                 total += id
+            #else:
+            #    print(f"{id}")
             
 print(f"Final: {total}")
