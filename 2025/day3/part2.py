536f7e95c30614dbc66e6a7fd0a918879d402eb6
Philip Heindel <p.e.heindel@gmail.com>
2025-12-03 21:50:42 -0500

    Completed 2025 day 3 part 2

--- 2025/day3/part2.py
+++ 2025/day3/part2.py
@@ -2,25 +2,28 @@ total = 0
 
 
 def get_max_joltage(number: str, digits: int) -> int:
-    largest = 0
-    print(f"{digits}{number}")
-    upper_limit = 0
+    final = ""
+    print(f"> {number}")
+    upper_limit = -1
     start_index = digits
     for i in range(len(number[digits:]), len(number)):
-        print(number[i])
-        for j in range(len(number) - digits, -1, -1):
-            print(number[j])
-            #print(f"{current}")
-            #if current > largest:
-            #    largest = current
-        print()
-    print(f"{largest}")
-    return largest
+        current_largest = int(number[i])
+        current_index = i
+        for j in range(i, upper_limit , -1):
+            current = int(number[j])
+            if current >= current_largest:
+                current_largest = current
+                current_index = j
+        upper_limit = current_index
+        final += str(current_largest)
+        print(f"j:{j} i:{i} {current_largest}")
+    print(f"inal")
+    return int(final)
 
 
-with open("test_input.txt") as file:
+with open("input.txt") as file:
     for line in file:
-        total += get_max_joltage(line.rstrip(), 2)
+        total += get_max_joltage(line.rstrip(), 12)
 
 
 print(f"Final: {total}")
