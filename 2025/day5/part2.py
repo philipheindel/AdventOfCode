b0b5d7ca1d4c99aab631687a2e833b2084fd29c7
Philip Heindel <p.e.heindel@gmail.com>
2025-12-05 20:15:04 +0000

    still working on part 2

--- 2025/day5/part2.py
+++ 2025/day5/part2.py
@@ -1,23 +1,54 @@
-total = 0
+import numpy as np
 
+total = 0
 
 ranges = []
 fresh_ids =[]
 
-
-with open("input.txt") as file:
+with open("test_input.txt") as file:
     for line in file:
         if line.rstrip() == "":
             break
         range_start = int(line.rstrip().split('-')[0])
-        range_end = int(line.rstrip().split('-')[1]) + 1
+        range_end = int(line.rstrip().split('-')[1])
         ranges.append([range_start, range_end])
-        comb = set(list(fresh_ids), list(range(range_start, range_end)))
-        fresh_ids = list(comb)
+
+ranges = sorted(ranges, key=lambda x: x[0])
+merged_ranges = []
+i = 0
+j = i + 1
+current_range = [ranges[0][0],ranges[0][1]]
+while i < len(ranges):
+    if j >= len(ranges):
+        break
+    current_start = ranges[i][0]
+    current_end = ranges[i][1]
+    next_start = ranges[j][0]
+    next_end = ranges[j][1]
+    print(f"i: {i}, j: {j}")
+    print(f"cs: {current_start}, ce: {current_end}\n ns: {next_start}, ne: {next_end}")
+    if current_end < next_start:
+        print(f"{current_end} < {next_start}")
+        i = j
+        j = i + 1
+        merged_ranges.append(current_range)
+        print(current_range)
+        current_range = [0,0]
+        # move on to next range
+        # update i and j indices 
         continue
-        for fresh_id in range(range_start, range_end):
-            if fresh_id not in fresh_ids:
-                fresh_ids.append(fresh_id)
+    if current_end >= next_start:
+        print(f"{current_end} >= {next_start}")
+        current_range[0] = current_start
+        current_range[1] = next_end
+        
+        j += 1
+        # merge current and next
+        # update j index
+        print(current_range)
+        continue
+
+print(merged_ranges)
 
 
 total = len(fresh_ids)
