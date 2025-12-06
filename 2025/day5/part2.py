734e213ff1bfdd83f4b5f10403256041d02fe496
Philip Heindel <p.e.heindel@gmail.com>
2025-12-05 22:41:47 -0500

    completed 2025 day5 part2

--- 2025/day5/part2.py
+++ 2025/day5/part2.py
@@ -1,11 +1,11 @@
-import numpy as np
+
 
 total = 0
 
 ranges = []
 fresh_ids =[]
 
-with open("test_input.txt") as file:
+with open("input.txt") as file:
     for line in file:
         if line.rstrip() == "":
             break
@@ -20,38 +20,36 @@ j = i + 1
 current_range = [ranges[0][0],ranges[0][1]]
 while i < len(ranges):
     if j >= len(ranges):
+        merged_ranges.append([current_range[0], current_range[1]])
         break
-    current_start = ranges[i][0]
-    current_end = ranges[i][1]
+    if j == (i + 1):
+        #print("initial")
+        current_range[0] = ranges[i][0]
+        current_range[1] = ranges[i][1]
     next_start = ranges[j][0]
     next_end = ranges[j][1]
-    print(f"i: {i}, j: {j}")
-    print(f"cs: {current_start}, ce: {current_end}\n ns: {next_start}, ne: {next_end}")
-    if current_end < next_start:
-        print(f"{current_end} < {next_start}")
+    #print(f"i: {i}, j: {j}")
+    #print(f"cs: {current_range[0]}, ce: {current_range[1]}\n ns: {next_start}, ne: {next_end}")
+    if current_range[1] < next_start:
+        #print("current")
+        #print(current_range)
+        merged_ranges.append([current_range[0], current_range[1]])
+        #print("merged")
+        #print(merged_ranges)
         i = j
         j = i + 1
-        merged_ranges.append(current_range)
-        print(current_range)
-        current_range = [0,0]
+        #print(current_range)
         # move on to next range
         # update i and j indices 
         continue
-    if current_end >= next_start:
-        print(f"{current_end} >= {next_start}")
-        current_range[0] = current_start
+    if current_range[1] >= next_start and current_range[1] < next_end:
         current_range[1] = next_end
-        
-        j += 1
-        # merge current and next
-        # update j index
-        print(current_range)
-        continue
+    j += 1
 
 print(merged_ranges)
 
-
-total = len(fresh_ids)
+for id_range in merged_ranges:
+    total += (id_range[1] - id_range[0] + 1)
 
 
 print(f"Final: {total}")
