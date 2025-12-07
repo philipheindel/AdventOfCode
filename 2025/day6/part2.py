aefb54cd9324976f21858f3340afd6b38989f8f3
Philip Heindel <p.e.heindel@gmail.com>
2025-12-07 03:28:51 +0000

    Completed 2025 day6 part2

--- 2025/day6/part2.py
+++ 2025/day6/part2.py
@@ -5,36 +5,43 @@ total = 0
 matrix = []
 problems = []
 
-with open("test_input.txt") as file:
+with open("input.txt") as file:
     for line in file:
         matrix.append(line.replace('\n',''))
 
 operations = matrix.pop()
 matrix.insert(0, operations)
     
-width = len(matrix[0].split())
+width = len(matrix[0])
 height = len(matrix)
+new_problem = []
+new_operand = ""
 
 for i in range(width):
     is_separator = True
     for j in range(height):
+        #print(f"matrix[{j}][{i}]:{matrix[j][i]}")
         if matrix[j][i] != ' ':
             is_separator = False
+            if matrix[j][i] == '+' or matrix[j][i] == '*':
+                new_problem.append(matrix[j][i])
+            else:
+                new_operand += matrix[j][i]
     if is_separator:
+        #print("separated")
+        is_separator = True
+        problems.append(new_problem)
+        new_problem = []
+    else:
+        #print(f"operand: {new_operand}")
+        new_problem.append(new_operand)
+        new_operand = ""
+problems.append(new_problem)
 
-        print(f"{j} {i}")
-
-print(matrix)
-
-exit()
-for i in range(width):
-    new_problem = []
-    for j in range(height):
-        new_problem.append(matrix[j][i])
-    problems.append(new_problem)
+#print(matrix)
+#print(problems)
 
 for i,problem in enumerate(problems):
-    problem.reverse()
     operation = problem[0]
     result = 0
     if operation == '*':
