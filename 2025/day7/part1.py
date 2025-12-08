d32956d568ec73124a696e4a4c16a66605538b5a
Philip Heindel <p.e.heindel@gmail.com>
2025-12-08 05:10:32 +0000

    Completed 2025 day7 part1

--- 2025/day7/part1.py
+++ 2025/day7/part1.py
@@ -1,6 +1,6 @@
 total = 0
 
-debugging = True
+debugging = False
 
 if debugging:
     input_file = "test_input.txt"
@@ -9,35 +9,40 @@ else:
 
 
 matrix = []
+splits = 0
 
 
-def debug(message: str) -> None:
+def debug(message: str, end: str = '\n') -> None:
     global debugging
     if debugging:
-        print(message)
+        print(message, end=end)
+        
 
 
 def eval_point(row: int, col: int) -> None:
     global matrix
+    global splits
     current_point = matrix[row][col]
     previous_point = matrix[row-1][col]
-    if matrix[row][col] == "." or matrix[row][col] == "^":
-        if matrix[row-1][col] == "." or matrix[row-1][col] == "^":
-            return
-        elif matrix[row-1][col] == "|":
-
-    if col-1 >= 0 and (matrix[row][col-1] == "@" or matrix[row][col-1] == "X"):
-        count += 1
-    #    print("@",end='')
-    #else:
-    #    print(".",end='')
-    if col+1 < len(matrix[row]) and (matrix[row][col+1] == "@" or matrix[row][col+1] == "X"):
-        count += 1
-    #    print("@")
-    #else:
-    #    print(".")
-    return count
-
+    if col-1 >= 0:
+        current_left = matrix[row][col-1]
+    if col+1 < len(matrix[row]):
+        current_right = matrix[row][col+1]
+    if current_point == "." and (previous_point == "." or previous_point == "^"):
+        return
+    if current_point == "." and previous_point == "|":
+        matrix[row][col] = "|"
+        return
+    if current_point == "^" and previous_point == "|":
+        if current_left == "." or current_right == ".":
+            debug(f"matrix[{row}][{col}] = {matrix[row][col]}")
+            splits += 1
+            matrix[row][col-1] = "|"
+            matrix[row][col+1] = "|"
+            matrix[row][col] = "+"
+    if previous_point == "S":
+        matrix[row][col] = "|"
+        return
 
 
 with open(input_file) as file:
@@ -45,17 +50,15 @@ with open(input_file) as file:
         matrix.append(list(line.rstrip()))
 
 for i,row in enumerate(matrix):
-    for j,element in enumerate(row):
+    for j,element in enumerate(matrix[i]):
         eval_point(i, j)
-        if element == ".":
-            debug(".",end='')
-            continue
-        adjacent_count = check_row(i+1,j)
-        adjacent_count += (check_row(i,j) - 1)
-        adjacent_count += check_row(i-1,j)
-        if adjacent_count < 4:
-            removable_count += 1
-            matrix[i][j] = "X"
+    # print(matrix[i])
 
+for i,row in enumerate(matrix):
+    for j,element in enumerate(matrix[i]):
+        debug(matrix[i][j], "")
+    debug("")
+
+total = splits
 
 print(f"Final: {total}")
